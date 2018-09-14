93b950a73674386714364e8ccb0bac0773c539a1
leomafraa <leonardomafra574@gmail.com>
2018-09-14 10:18:48 -0300

    Responsive

--- assets/src/js/main.js
+++ assets/src/js/main.js
@@ -4,7 +4,13 @@ const testimonialCount = testimonialBullets.length;
 const testimonials = document.getElementById('testimonials-wrapper');
 const autoPlayInterval = 4000;
 
-testimonials.style.width = testimonialWidth * testimonialCount + 'px';
+if (document.body && document.body.offsetWidth) {
+  testimonials.style.width = testimonialWidth * testimonialCount + 'px';
+}
+
+if (window.innerWidth) {
+  testimonials.style.width = testimonialWidth * testimonialCount + 'px';
+}
 
 const goToTestimonial = (index) => {
   for (let bullet of testimonialBullets) {
